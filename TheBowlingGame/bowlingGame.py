a56ff7348e5bce83d7a7c4c65cde53fc72c557f8
Ali Payne <payneal@onid.oregonstate.edu>
2015-09-17 23:44:33 -0400

    passed gutter ball test

--- TheBowlingGame/bowlingGame.py
+++ TheBowlingGame/bowlingGame.py
@@ -1,3 +1,9 @@
-#initially empty
+class Game(object): 
+	def __init__(self):
+		self.stats = []
 
+	def roll(self, pins): 
+		self.stats.append(pins)
 
+	def score(self): 
+		return 0
--- TheBowlingGame/bowlingGameTest.py
+++ TheBowlingGame/bowlingGameTest.py
@@ -1,8 +1,19 @@
 import unittest
 
+from bowlingGame import Game
+
 class TestBowlingGame(unittest.TestCase):
-	def testStartgame (self): 
-		game = bowlingGame() 
+	
+	def test_Start_a_game (self): 
+		game  = Game() 
+
+	def test_to_check_gutter_game(self):
+		game = Game()
+		for x in range(20): 
+			game.roll(0)
+
+		self.assertEqual(game.score(), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
