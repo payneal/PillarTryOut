cbc1d0ae7be7c4c1488d2f8d7f47cfe59155c676
Ali Payne <payneal@onid.oregonstate.edu>
2015-09-18 15:53:13 -0400

    passed roll one spear

--- TheBowlingGame/bowlingGame.py
+++ TheBowlingGame/bowlingGame.py
@@ -1,13 +1,21 @@
 class Game(object): 
 	def __init__(self):
+		self.frame = 10
+		self.spearOrStrike = 10
 		self.stats = []
+		
 
 	def roll(self, pins): 
 		self.stats.append(pins)
 
 	def score(self): 
 		score = 0
-		for x in self.stats: 
-			score = score + x
+		rollIndex = 0
+		for x in range(self.frame):
 
+			if self.stats[rollIndex] + self.stats[rollIndex+1] == self.spearOrStrike: 
+				score += self.stats[rollIndex] + self.stats[rollIndex+1] + self.stats[rollIndex+2]
+			else: 
+				score += self.stats[rollIndex] + self.stats[rollIndex+1]
+			rollIndex += 2
 		return score
--- TheBowlingGame/bowlingGameTest.py
+++ TheBowlingGame/bowlingGameTest.py
@@ -7,14 +7,24 @@ class TestBowlingGame(unittest.TestCase):
 		self.game = Game()  
 
 	def test_to_check_all_gutter_balls_game(self):
-		for x in range(20): 
-			self.game.roll(0)
+		self.roll_this_many_times_hit_this_many_pins(20, 0)
 		self.assertEqual(self.game.score(), 0)
 
 	def test_to_check_all_ones(self):
-		for x in range(20): 
-			self.game.roll(1)
+		self.roll_this_many_times_hit_this_many_pins(20, 1)
 		self.assertEqual(self.game.score(), 20)
+
+	def test_to_check_rolling_one_spare(self):
+		self.game.roll(7)
+		self.game.roll(3)
+		self.game.roll(5)
+		self.roll_this_many_times_hit_this_many_pins(17, 0)
+		self.assertEqual(self.game.score(), 20)
+
+
+	def roll_this_many_times_hit_this_many_pins(self, rolls, pinsHit): 
+		for x in range(rolls): 
+			self.game.roll(pinsHit)
 
 
 if __name__ == '__main__':
