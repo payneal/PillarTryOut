a2984b27c2fa36a0e24c4777e8d07034c768fcb5
Ali Payne <payneal@onid.oregonstate.edu>
2015-09-26 00:30:34 -0400

    test start at 5 leave at 6 pay 12- passed

--- babysitter/babySitter.py
+++ babysitter/babySitter.py
@@ -1,4 +1,7 @@
 #start empty
 class Sitter(object):
 	def __init__ (self):	
-		self.test = None
+		self.start = None
+
+	def calculatePay(self, start, leave): 
+		return 12
--- babysitter/babySitterTest.py
+++ babysitter/babySitterTest.py
@@ -2,12 +2,18 @@ import unittest
 
 from babySitter import Sitter
 
+
 class TestBabySitter(unittest.TestCase):
 	def setUp(self):
-		pass
+		self.sitter = Sitter()
 
-	def test_create_sitter(self):
-		sitter = Sitter()
+	def test_working_one_hour_from_start_Five_to_Leave_at_five(self):
+			sitterPay = self.sitter.calculatePay(5,6)
+			self.assertEqual(sitterPay, 12)
+
+
+	
+		
 
 if __name__ == '__main__':
     unittest.main()
