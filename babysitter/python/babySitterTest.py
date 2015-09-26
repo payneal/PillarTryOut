0855750707a567b012680001db84f2e213a080af
Ali Payne <payneal@onid.oregonstate.edu>
2015-09-26 12:32:34 -0400

    changed folders format

--- babysitter/python/babySitterTest.py
+++ babysitter/python/babySitterTest.py
@@ -7,12 +7,27 @@ class TestBabySitter(unittest.TestCase):
 	def setUp(self):
 		self.sitter = Sitter()
 
-	def test_working_one_hour_from_start_Five_to_Leave_at_five(self):
-			sitterPay = self.sitter.calculatePay(5,6)
-			self.assertEqual(sitterPay, 12)
+	def test_startTime_5_leaveTime_6_bedTime_7_hours_1_pay_12(self):
+		sitterPay = self.sitter.calculatePay(5,6,7)
+		self.assertEqual(sitterPay, 12)
+
+	def test_startTime_5_leaveTime_7_bedtime_8_hours_2_pay_24(self):
+		sitterPay = self.sitter.calculatePay(5,7,8)
+		self.assertEqual(sitterPay, 24)
+
+	def test_startTime_11_leaveTime_12_bedtime_11_hours_1_pay_8(self):
+		sitterPay = self.sitter.calculatePay(11,12,11)
+		self.assertEqual(sitterPay, 8)
+
+	def test_startTime_10_leaveTime_12_bedtime_10_hours_2_bedTimeToMidnight_pay_16(self):
+		sitterPay = self.sitter.calculatePay(10,12,10)
+		self.assertEqual(sitterPay, 16)
 
 
-	
+
+
+
+
 		
 
 if __name__ == '__main__':
