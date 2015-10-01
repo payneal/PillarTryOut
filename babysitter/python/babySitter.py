1124ca281b17e52f7cc5d60c02737f384abdc931
Ali Payne <payneal@onid.oregonstate.edu>
2015-10-01 07:34:42 -0400

    passed no baby sitting after 4am(not refracted

--- babysitter/python/babySitter.py
+++ babysitter/python/babySitter.py
@@ -6,8 +6,35 @@ class Sitter(object):
 		self.midnightPay = 16
 
 	def calculatePay(self, startTime, leaveTime, bedTime): 
-		if startTime < 5: 
+
+		amOrPMStart = None
+		if "AM" in startTime: 
+			amOrPMStart = "AM"
+			startTime = startTime.replace("AM", "")
+		else:
+			amOrPMStart = "PM"
+			startTime = startTime.replace("PM", "")
+		
+		startTime = int(startTime)
+
+		amOrPMLeave = None
+		if "AM" in leaveTime: 
+			amOrPMLeave = "AM"
+			leaveTime = leaveTime.replace("AM", "")
+		else:
+			amOrPMLeave = "PM"
+			leaveTime =  leaveTime.replace("PM", "")
+		
+		leaveTime = int(leaveTime)
+
+		bedTime = bedTime.replace("PM", "")
+		bedTime = int(bedTime)
+		
+
+		if startTime < 5:
 			raise Exception("Baby sitting starts no earlier than 5:00PM") 
+		elif leaveTime > 4 and leaveTime != 12 and amOrPMLeave == "AM" : 
+			 raise Exception("Baby sitting ends no later than 4:00AM") 
 		elif startTime == 12:
 			return ((12+leaveTime) - startTime) * self.midnightPay
 		elif startTime != bedTime:
--- babysitter/python/babySitterTest.py
+++ babysitter/python/babySitterTest.py
@@ -8,35 +8,38 @@ class TestBabySitter(unittest.TestCase):
 		self.sitter = Sitter()
 
 	def test_startTime_5_leaveTime_6_bedTime_7_hours_1_pay_12(self):
-		sitterPay = self.sitter.calculatePay(5,6,7)
+		sitterPay = self.sitter.calculatePay("5PM","6PM","7PM")
 		self.assertEqual(sitterPay, 12)
 
 	def test_startTime_5_leaveTime_7_bedtime_8_hours_2_pay_24(self):
-		sitterPay = self.sitter.calculatePay(5,7,8)
+		sitterPay = self.sitter.calculatePay("5PM","7PM","8PM")
 		self.assertEqual(sitterPay, 24)
 
 	def test_throws_execption_error_if_babysitter_start_time_before_5(self):
 		with self.assertRaises(Exception) as context:
-			self.sitter.calculatePay(4,6,7)
+			self.sitter.calculatePay("4PM","6PM","7PM")
 		self.assertTrue("Baby sitting starts no earlier than 5:00PM" in context.exception)
 
 	def test_startTime_11_leaveTime_12_bedtime_11_hours_1_pay_8(self):
-		sitterPay = self.sitter.calculatePay(11,12,11)
+		sitterPay = self.sitter.calculatePay("11PM","12AM","11PM")
 		self.assertEqual(sitterPay, 8)
 
 	def test_startTime_10_leaveTime_12_bedtime_10_hours_2_bedTimeToMidnight_pay_16(self):
-		sitterPay = self.sitter.calculatePay(10,12,10)
+		sitterPay = self.sitter.calculatePay("10PM","12AM","10PM")
 		self.assertEqual(sitterPay, 16)
 
 	def test_startTime_12am_leaveTime_1_bedtime_11_hours_1_testing_16_hour_pay_from_12am_to_end_of_Job(self): 
-		sitterPay = self.sitter.calculatePay(12,1,11)
+		sitterPay = self.sitter.calculatePay("12AM","1AM","11PM")
 		self.assertEquals(sitterPay, 16)
 
 	def test_startTime_12am_leaveTime_2_bedtime_11_hours_2_testing_32_hour_pay_from_12am_to_end_of_Job(self):
-		sitterPay = self.sitter.calculatePay(12,2,11)
+		sitterPay = self.sitter.calculatePay("12AM","2AM","11PM")
 		self.assertEquals(sitterPay, 32)		
 	
-
+	def test_throws_execption_error_if_babysitter_leave_time_after_4(self):
+		with self.assertRaises(Exception) as context:
+			self.sitter.calculatePay("12AM","5AM","11PM")
+		self.assertTrue("Baby sitting ends no later than 4:00AM" in context.exception)
 
 if __name__ == '__main__':
     unittest.main()
