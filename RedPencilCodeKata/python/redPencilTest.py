87fdebe2ebb324d873605b98c48ad0e40b692641
Ali Payne <Ali@alis-air.attlocal.net>
2015-10-13 04:03:09 -0400

    test passed added 2 items with name and price

--- RedPencilCodeKata/python/redPencilTest.py
+++ RedPencilCodeKata/python/redPencilTest.py
@@ -6,17 +6,11 @@ class TestRedPencil(unittest.TestCase):
 	def setUp(self):
 		self.store = Shop()
 
-	def test_adding_item_to_store(self): 
-		
-		self.store.addItem('shoe')
+	def test_adding_two_items_with_prices(self):
+		self.store.addItem({'shoe':5.00})
+		self.store.addItem({'tie':4.00})
 		items = self.store.getAllItems()
-		self.assertEqual(items, ["shoe"])
-
-	def test_adding_two_items_to_store(self): 
-		self.store.addItem('shoe')
-		self.store.addItem('tie')
-		items = self.store.getAllItems()
-		self.assertEqual(items, ["shoe", "tie"])
+		self.assertEqual(items, [{'shoe':5.00}, {'tie':4.00}])
 
 if __name__ == '__main__':
     unittest.main()
