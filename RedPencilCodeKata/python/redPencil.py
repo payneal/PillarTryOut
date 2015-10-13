864c7dae975222d7307cd416d2deba60bd2ed55b
payneal <ali.n.payne@gmail.com>
2015-10-13 17:36:41 -0400

    passed test changing price by 5 percent sparks red pencil status .... time to refactor

--- RedPencilCodeKata/python/redPencil.py
+++ RedPencilCodeKata/python/redPencil.py
@@ -1,8 +1,12 @@
 
 class item(object):
 	def __init__(self, name, price):
-		 self.name = name
-		 self.price = price 
+		self.name = name
+		self.price = price 
+		self.redPencileStatus = False
+
+	def changeRedPencileStatus(self):
+		self.redPencileStatus = True
 		 
 class Shop(object): 
 	def __init__(self): 
@@ -20,10 +24,20 @@ class Shop(object):
 		return allItems
 
 	def changePrice(self, name, price):
+		previousPrice= None
+
 		for x in self.inStoreItem:
 			if x.name == name:
+				previousPrice = x.price
 				x.price = price
 
+				if price <= (previousPrice - (previousPrice * .05)):
+					x.changeRedPencileStatus()
+					
+	
 	def isOnRedPencileSale(self,name): 
+		for x in self.inStoreItem:
+			if x.name == name:
+				return x.redPencileStatus 
 		return False
 
