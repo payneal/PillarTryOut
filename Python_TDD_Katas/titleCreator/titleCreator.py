ddef6f1c793cdc5c67fba1bd6fd07be0ebf6d4dc
payneal <ali.payne@zipscene.com>
2016-06-07 11:31:25 -0400

    passed first test case

--- Python_TDD_Katas/titleCreator/titleCreator.py
+++ Python_TDD_Katas/titleCreator/titleCreator.py
@@ -1,1 +1,11 @@
+# Class
 
+class TitleCreator:
+
+
+    def __init__(self, string, lettersToCap=None):
+        self.string = string
+        self.letters = lettersToCap
+
+    def titleCreator(self):
+        return self.string
--- Python_TDD_Katas/titleCreator/titleCreatorTest.py
+++ Python_TDD_Katas/titleCreator/titleCreatorTest.py
@@ -1,1 +1,14 @@
+# Tests
+import unittest
+from titleCreator import TitleCreator
 
+class TestStockHighNyse(unittest.TestCase):
+    
+    
+    def test_if_one_argument_passed__then_return_same_string(self):
+        string = '';
+        createdTitle = TitleCreator(string)
+        self.assertEqual(string, createdTitle.titleCreator())
+    
+if __name__ == '__main__':
+    unittest.main()
