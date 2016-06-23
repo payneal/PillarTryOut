af3951880adfc3ae5b4c8e1aa8bb577c84256f30
Ali Payne <payneal@onid.oregonstate.edu>
2016-06-22 23:40:16 -0400

    completed weird string

--- JS_TDD_Katas/weirdStringCase/tests/weirdStringCaseTest.js
+++ JS_TDD_Katas/weirdStringCase/tests/weirdStringCaseTest.js
@@ -11,5 +11,25 @@ describe("weird case string", function() {
             let weirdString = new WeirdStringCase();
             expect(weirdString.toWeirdCase('I')).to.equal('I');
         });
+        it('one letter(not capped) should just return same string(capped)',function(){
+            let weirdString = new WeirdStringCase();
+            expect(weirdString.toWeirdCase('i')).to.equal('I');
+        });
+        it('2 letters(capped, not) should just return same string',function(){
+            let weirdString = new WeirdStringCase();
+            expect(weirdString.toWeirdCase('It')).to.equal('It');
+        });
+        it('3 letters(capped, not, not ) should just return same string(capped, not, capped)',function(){
+            let weirdString = new WeirdStringCase();
+            expect(weirdString.toWeirdCase('Its')).to.equal('ItS');
+        });
+        it('is a => Is A',function(){
+            let weirdString = new WeirdStringCase();
+            expect(weirdString.toWeirdCase('is a')).to.equal('Is A');
+        });
+        it('This is a test => ThIs Is A Test',function(){
+            let weirdString = new WeirdStringCase();
+            expect(weirdString.toWeirdCase('This is a test')).to.equal('ThIs Is A TeSt');
+        }); 
     });
 });
--- JS_TDD_Katas/weirdStringCase/weirdStringCase.js
+++ JS_TDD_Katas/weirdStringCase/weirdStringCase.js
@@ -1,9 +1,41 @@
 "use strict";
 
 function WeirdStringCase(){
-    this.toWeirdCase = function(string) { 
-        return string;
-    };
-}
+    this._string = null;
+    this._caps = true;
+    this._count = 0;
+    this._capsCount = 0;
 
+    this.toWeirdCase = function(string) {
+        this._string = string.split('');
+        _cycleThroughLettersToBeChanged.call(this);
+        return this._string.join('');
+    };
+
+    let _cycleThroughLettersToBeChanged = function(){
+        for (let letter of this._string) {
+            _shouldLettersBeCapped.call(this, letter); 
+            this._count++;
+            this._capsCount++;
+            _changeCapsOrNot.call(this);
+        }
+    };
+
+    let _shouldLettersBeCapped = function(letter){ 
+        if (this._caps === true && letter !== ' ') {
+            this._string[this._count] = letter.toUpperCase();
+            this._caps = false;
+        } 
+        if (letter == ' ') {
+            this._capsCount = -1;
+        }
+    };
+
+    let _changeCapsOrNot = function() {
+        if (this._capsCount%2 === 0){
+            this._caps = true;
+        }
+    };
+
+}
 module.exports = WeirdStringCase;
