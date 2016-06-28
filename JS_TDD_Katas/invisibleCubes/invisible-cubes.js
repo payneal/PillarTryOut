0b57c485ba008cb3db236e60a2a708cf63aa5a4a
Ali Payne <payneal@onid.oregonstate.edu>
2016-06-28 07:51:19 -0400

    started new kata and failed 2nd test

---
+++ JS_TDD_Katas/invisibleCubes/invisible-cubes.js
@@ -0,0 +1,7 @@
+"use strict";
+function InvisibleCubes(){
+    this.notVisibleCubes = function(x) {
+        return 0
+    };
+}
+module.exports = InvisibleCubes;
---
+++ JS_TDD_Katas/invisibleCubes/tests/invisible-cubes-tests.js
@@ -0,0 +1,15 @@
+var chai = require('chai');
+var expect = chai.expect ; 
+var InvisibleCubes = require('../invisible-cubes');
+
+describe('Invisible Cubes', function() {
+    var invisibleCubes = new InvisibleCubes();
+    it('entering 1 rows should return 0 not visible from outside', function(){
+        var notVisible = invisibleCubes.notVisibleCubes(1); 
+        expect(notVisible).to.eql(0);
+    });
+    it('entering 3 rows should return 1 not visible from outside', function(){
+        var notVisible = invisibleCubes.notVisibleCubes(3); 
+        expect(notVisible).to.eql(1);
+    });
+});
