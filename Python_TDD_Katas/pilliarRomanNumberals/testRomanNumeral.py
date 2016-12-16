2c2d2b18e6981c2198831808044cbc221b4a4e01
Ali Payne <payneal@onid.oregonstate.edu>
2016-12-16 00:16:13 -0500

    test failed 3 -> III

--- Python_TDD_Katas/pilliarRomanNumberals/testRomanNumeral.py
+++ Python_TDD_Katas/pilliarRomanNumberals/testRomanNumeral.py
@@ -12,5 +12,8 @@ class TestRomalNumeralConverter(unittest.TestCase):
     def test_convert_1_to_I(self):
         self.assertEqual(self.romanNumeral.convert(1), "I")
 
+    def test_convert_3_to_III(self):
+        self.assertEqual(self.romanNumeral.convert(3), "III")
+
 if __name__ == '__main__':
     unittest.main()
