69abdf040f45a5bf07f1b29201cb95b45384ce40
Ali Payne <payneal@onid.oregonstate.edu>
2016-12-16 00:43:30 -0500

    failed 9 -> IX

--- Python_TDD_Katas/pilliarRomanNumberals/testRomanNumeral.py
+++ Python_TDD_Katas/pilliarRomanNumberals/testRomanNumeral.py
@@ -15,5 +15,8 @@ class TestRomalNumeralConverter(unittest.TestCase):
     def test_convert_3_to_III(self):
         self.assertEqual(self.romanNumeral.convert(3), "III")
 
+    def test_convert_9_to_IX(self):
+        self.assertEqual(self.romanNumeral.convert(9), "IX")
+
 if __name__ == '__main__':
     unittest.main()
