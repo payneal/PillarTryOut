9b25716c45b7ac0d063986d0dbd0e6dd8b4607bd
Ali Payne <payneal@onid.oregonstate.edu>
2016-12-19 01:48:30 -0500

    passed test 4 -> IV

--- Python_TDD_Katas/pilliarRomanNumberals/romanNumeral.py
+++ Python_TDD_Katas/pilliarRomanNumberals/romanNumeral.py
@@ -1,13 +1,29 @@
 class RomanNumeral:
     def __init__(self):
         self.converted = ''
+        self.roman = [{'letter': 'I', 'value': 1, 'limit': 3},
+                      {'letter': 'V', 'value': 5, 'limit': 1},
+                      {'letter': 'X', 'value': 10, 'limit': 3}]
 
-    def convert(self, entry):
-        for x in range(0, entry):
-            self.converted += "I"
-        return self.__check()
-
-    def __check(self):
-        if len(self.converted) > 3:
-            return "IX"
+    def convert(self, number):
+        indexOfRoman = self.__findClosestValueToNumber(number)
+        if number == self.roman[indexOfRoman]['value']:
+            self.converted = self.roman[indexOfRoman]['letter']
+        else:
+            self.__createConversionWithLowerRoman(indexOfRoman, number)
         return self.converted
+
+    def __findClosestValueToNumber(self, number):
+        for index, x in enumerate(self.roman):
+            if x['value'] >= number:
+                return index
+
+    def __createConversionWithLowerRoman(self, indexOfRoman, number):
+        if number <= self.roman[indexOfRoman - 1]['limit']:
+            for x in range(0, number):
+                self.converted += "I"
+        else:
+            self.converted += self.roman[indexOfRoman]['letter']
+            difference = self.roman[indexOfRoman]['value'] - number
+            for x in range(0, difference):
+                self.converted = "I" + self.converted
